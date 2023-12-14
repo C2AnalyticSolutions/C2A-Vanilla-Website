88fb3a43d5b164aea9c8da94d8f7188f5a78d966
LuiiVuitton <Luis1134@icloud.com>
2023-12-13 20:21:47 -0500

    finished contact us section

--- c2a-portfolio-draft/script.js
+++ c2a-portfolio-draft/script.js
@@ -1,36 +1,17 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
-
-
-//Scroll(comming soon...)
-// var prevScrollPos = window.pageYOffset;
-// var stickyNav = document.getElementById('sticky-nav');
-
-// window.addEventListener('scroll', function() {
-//     var currentScrollPos = window.pageYOffset;
-    
-//     if (currentScrollPos > prevScrollPos) {
-//         // Scrolling down, hide the sticky navigation
-//         stickyNav.style.display = 'none';
-//     } else {
-//         // Scrolling up, show the sticky navigation
-//         stickyNav.style.display = 'block';
-//     }
-    
-//     prevScrollPos = currentScrollPos;
-// });
-
-
 
 // Get the button: (go to top settings)
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -47,18 +28,16 @@ function topFunction() {
 }
 
 //adjust the vbideo container to fit it's container without expanding past it
-  window.addEventListener("load", function() {
-    // Get references to the video and container elements
-    const video = document.querySelector(".article-container video");
-    const container = document.querySelector(".article-container");
+window.addEventListener("load", function () {
+  // Get references to the video and container elements
+  const video = document.querySelector(".article-container video");
+  const container = document.querySelector(".article-container");
 
-    // Calculate the container's height based on the video's aspect ratio
-    const videoAspectRatio = video.videoWidth / video.videoHeight;
-    const containerWidth = container.offsetWidth;
-    const containerHeight = containerWidth / videoAspectRatio;
+  // Calculate the container's height based on the video's aspect ratio
+  const videoAspectRatio = video.videoWidth / video.videoHeight;
+  const containerWidth = container.offsetWidth;
+  const containerHeight = containerWidth / videoAspectRatio;
 
-    // Set the container's height
-    container.style.height = `${containerHeight}px`;
-  });
-
-
+  // Set the container's height
+  container.style.height = `${containerHeight}px`;
+});
